399b5838711c9a48ff89e061c16fdf4a3057c5a2
Vishal <vishalpawar048@gmail.com>
2025-11-30 10:55:22 +0530

    updated vercel

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
-import { createAuthClient } from "better-auth/react"
+import { createAuthClient } from "better-auth/react";
 
 export const authClient = createAuthClient({
-    baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000" 
-})
-
+  baseURL: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
+  secret: process.env.BETTER_AUTH_SECRET as string,
+});
